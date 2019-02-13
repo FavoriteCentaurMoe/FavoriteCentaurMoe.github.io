d88ac6c345583481643e2c3ce208cc72d3e263e3
Nikhil Sharma <nik41507@gmail.com>
2019-02-13 01:18:05 -0800

    Template Integrated

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import jquery from 'jquery'
+require('bootstrap')
+require('jquery.easing')
+require('bootstrap/dist/css/bootstrap.css')
+require('devicons/css/devicons.css')
+require('font-awesome/css/font-awesome.css')
+require('./resume.css')
+
+
+
+Vue.prototype.jquery = jquery
 
 Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App)
+})
